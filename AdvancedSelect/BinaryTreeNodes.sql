77167db2f1e1f396ada568e512a28abfa6d5d992
Chloe <cehan16@gmail.com>
2017-10-30 13:10:14 -0400

    30Oct

--- AdvancedSelect/BinaryTreeNodes.sql
+++ AdvancedSelect/BinaryTreeNodes.sql
@@ -4,3 +4,10 @@ SELECT N, CASE WHEN P IS NULL THEN 'Root'
                 END
 FROM BST AS B
 ORDER BY N
+/*USE EXISTS*/
+SELECT N, CASE WHEN P IS NULL THEN 'Root'
+                WHEN EXISTS (SELECT * FROM BST WHERE P = B.N) THEN 'Inner'
+                ELSE 'Leaf'
+                END
+FROM BST AS B
+ORDER BY N
